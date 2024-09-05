b8f227f9106da3f535979b7825fd5d5156666897
PauloMats <pm.mats98@gmail.com>
2024-09-05 14:02:02 -0300

    Feat: Requisito 7 - Regra de habilitação do botão entrar

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -2,11 +2,31 @@ package com.betrybe.sociallogin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    private val loginBtn: Button by lazy { findViewById(R.id.login_button) }
+    private val emailText: TextInputLayout by lazy { findViewById(R.id.email_text_input_layout) }
+    private val passwordText: TextInputLayout by lazy { findViewById(R.id.password_text_input_layout) }
+    private val main: View by lazy { findViewById(R.id.main) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        emailText.editText?.addTextChangedListener {
+            val email = emailText.editText?.text.toString()
+            val password = passwordText.editText?.text.toString()
+            loginBtn.isEnabled = email.isNotEmpty() && password.isNotEmpty()
+        }
+
+        passwordText.editText?.addTextChangedListener {
+            val email = emailText.editText?.text.toString()
+            val password = passwordText.editText?.text.toString()
+            loginBtn.isEnabled = email.isNotEmpty() && password.isNotEmpty()
+        }
     }
 }
